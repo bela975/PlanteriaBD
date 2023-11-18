d0a74b8e850f8ef21b7b8abfb678f2caf50395aa
bela975 <isfsca@cesar.school>
2023-11-18 20:48:00 -0300

    adding streamlit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Planteria Oficial", page_icon=":tada:", layout= "wide")
+st.subheader("a melhor loja de plantas do planeta")
+st.title("feito por bela, maju e leleco")
+
+
